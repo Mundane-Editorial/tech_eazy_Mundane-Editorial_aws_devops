8c527f1febe420a58ea1f3e4d8697c3d702a3772
mundane-editorial <uday2k17@rediffmail.com>
2025-06-07 21:04:37 +0530

    variable initialization for developer configuration

---
+++ ec2_deployment/config/dev_config.tfvars
@@ -0,0 +1,6 @@
+aws_region = "ap-south-1"
+ami_id = "ami-09299e47e83cceaa7" 
+instance_type = "t2.micro"
+java_version = "19"
+shutdown_threshold = 20
+repo_url = "https://github.com/techeazy-consulting/techeazy-devops"
---
+++ ec2_deployment/variables.tf
@@ -0,0 +1,34 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1" # Mumbai
+}
+
+variable "ami_id" {
+  description = "ami id"
+  type        = string
+  default     = "ami-09299e47e83cceaa7" # amazon linux 2 - free tier eligible
+}
+
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "shutdown_threshold" {
+  description = "Machine Shutdown Threshold"
+  type        = number
+  default     = 20
+}
+
+varibale "java_version" {
+  description = "Java version"
+  type        = string
+  default     = "19"
+}
+
+variable "repo_url" {
+  description = "Github repository url"
+  type        = string
+}
