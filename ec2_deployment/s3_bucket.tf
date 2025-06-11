124cc3418acdcf53b4aa34edc586ee5db785830b
mundane-editorial <uday2k17@rediffmail.com>
2025-06-11 09:35:36 +0530

    force destroy attribute set to true -- for development purpose only

--- ec2_deployment/s3_bucket.tf
+++ ec2_deployment/s3_bucket.tf
@@ -4,6 +4,9 @@ resource "random_id" "suffix" {
 
 resource "aws_s3_bucket" "artifact_bucket" {
   bucket = "${var.bucket_name}-${random_id.suffix.hex}"
+
+  force_destroy = true
+
   tags = {
     Name        = var.bucket_name
     Environment = "Development"
