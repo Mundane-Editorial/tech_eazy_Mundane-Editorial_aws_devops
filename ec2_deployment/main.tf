879a5d7f31f830a1934f26b13bb6772260bc629f
mundane-editorial <uday2k17@rediffmail.com>
2025-06-18 19:39:29 +0530

    deploy.yml fix 4 :: key auto generation

--- ec2_deployment/main.tf
+++ ec2_deployment/main.tf
@@ -1,12 +1,29 @@
-data "aws_key_pair" "existing_key" {
-  key_name = var.ec2_key_name
+resource "null_resource" "generate_ssh_key" {
+  provisioner "local-exec" {
+    command = <<EOT
+      mkdir -p ${path.module}/key_pair
+      if [ ! -f "${path.module}/key_pair/id_rsa" ]; then
+        ssh-keygen -t rsa -b 2048 -f ${path.module}/key_pair/id_rsa -q -N ""
+      fi
+    EOT
+  }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
+
+resource "aws_key_pair" "generated_key" {
+  depends_on = [null_resource.generate_ssh_key]
+  key_name   = var.ec2_key_name
+  public_key = file("${path.module}/key_pair/id_rsa.pub")
 }
 
 resource "aws_instance" "Java-Application" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  key_name               = aws_key_pair.existing_key.key_name
-  vpc_security_group_ids = aws_security_group.Java-Application-SG.id
+  key_name               = aws_key_pair.generated_key.key_name
+  vpc_security_group_ids = [aws_security_group.Java-Application-SG.id]
   iam_instance_profile   = aws_iam_instance_profile.Java-Application-Profile.name
 
   provisioner "file" {
@@ -16,7 +33,7 @@ resource "aws_instance" "Java-Application" {
     connection {
       type        = "ssh"
       user        = var.instance_user
-      private_key = var.ec2_private_key
+      private_key = file("${path.module}/key_pair/id_rsa")
       host        = self.public_ip
     }
   }
@@ -30,14 +47,13 @@ resource "aws_instance" "Java-Application" {
     connection {
       type        = "ssh"
       user        = var.instance_user
-      private_key = var.ec2_private_key
+      private_key = file("${path.module}/key_pair/id_rsa")
       host        = self.public_ip
     }
   }
 
   tags = {
-    Name = "Java-Application-${var.stage}"
+    Name        = "Java-Application-${var.stage}"
     Environment = var.stage
   }
 }
-
